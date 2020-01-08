359e24ddd5502d11be9095e64c45b34c9faedd00
Daan645 <daan.jacobs@student.rocvf.nl>
2020-01-08 14:04:20 +0100

    Hoofdruk 2 afgerond

---
+++ H02/Opdracht 3.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn gweledige sukop";
+document.getElementById("aap1").src = "../Resources/aap1.jpg";
+document.getElementById("aap2").src = "../Resources/aap2.jpg";
